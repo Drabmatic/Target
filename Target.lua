ca301190e61d6da7076220e86a782042081b9956
Drabmatic <157862756+Drabmatic@users.noreply.github.com>
2024-02-07 20:57:39 -0500

    Added option for class color icons  	Added different sizes for the icons  	Added input fields for x & y
    Added option for class color icons
    
    Added different sizes for the icons
    
    Added input fields for x & y

--- Target.lua
+++ Target.lua
@@ -5,29 +5,18 @@ local defaultSettings =
 {
     xValue = 0,
     yValue = 0,
-    transparent = true
+    transparent = true,
+    useClassColor = false,
+    iconSize = "default" -- default icon size preset
 }
 
--- Initialize your Saved Variables table
--- Target_Settings = Target_Settings or CopyTable(defaultSettings) -- Moved to PLAYER_LOGIN
-
--- Auto generating the name based on class
--- local classTextureFiles = 
--- {
---     warrior     = "warrior",
---     hunter      = "hunter",
---     mage        = "mage",
---     rogue       = "rogue",
---     druid       = "druid",
---     paladin     = "paladin",
---     shaman      = "shaman",
---     priest      = "priest",
---     deathknight = "deathknight",
---     warlock     = "warlock",
---     monk        = "monk",
---     demonhunter = "demonhunter",
---     evoker      = "evoker"
--- }
+local presetSizes = {
+    small = {width = 10, height = 10},
+    medium = {width = 20, height = 20},
+    default = {width = 32, height = 32},
+    bigger = {width = 40, height = 40},
+    biggest = {width = 50, height = 50}
+}
 
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("ADDON_LOADED")
@@ -48,19 +37,23 @@ function createPlayer(unitId)
             player.class = player.class:lower()
         end
 
-        -- auto generate class image based on class name and transparency user setting.
-        local classImage = player.class .. (Target_Settings.transparent and "-circle" or "") .. ".tga"
+        local classImage
+        if Target_Settings.useClassColor then
+            classImage = player.class .. "-color" .. (Target_Settings.transparent and "-circle" or "") .. ".tga"
+        else
+            classImage = player.class .. (Target_Settings.transparent and "-circle" or "") .. ".tga"
+        end
 
         player.texture  = frame:CreateTexture(player.guid .. "-Texture", "OVERLAY")
+        local iconSize = presetSizes[Target_Settings.iconSize] or presetSizes["default"] -- fallback to default if preset not found
         player.texture:SetTexture("Interface\\AddOns\\" .. addonName .. "\\" .. classImage)
-        player.texture:SetSize(32, 32)
+        player.texture:SetSize(iconSize.width, iconSize.height)
     end
 
     return player
 end
 
 function clearPlayers(players)
-    -- clear out existing player textures
     if players then
         for unitId, player in pairs(players) do
             if player.texture then
@@ -75,7 +68,6 @@ end
 local players = {}
 
 function initializePlayers()
-    -- recreate roster
     clearPlayers(players)
 
     players["player"] = createPlayer("player")
@@ -91,13 +83,11 @@ function OnEvent(self, event, ...)
         initializePlayers()
     elseif event == "ADDON_LOADED" and arg1 == addonName then
     elseif event == "PLAYER_LOGIN" then
-        -- Load the saved settings
         if not Target_Settings then
             Target_Settings = CopyTable(defaultSettings)
         end
 
         initializeUI()
-        -- Initial call to updateNamePlates
         updateNamePlates()
     end
 end
@@ -117,7 +107,6 @@ end
 local nameplateFrames = {}
 
 function updateNamePlates(self)
-    -- remove current frames
     for key, value in pairs(nameplateFrames) do
         value:Hide()
         nameplateFrames[key] = nil
@@ -125,19 +114,16 @@ function updateNamePlates(self)
 
     local currentCounts = {}
     for unitId, player in pairs(players) do
-        -- target is showing on personal resource bar if i target myself :)
         if UnitExists(unitId) and UnitExists(player.targetId) and not UnitIsUnit("target", "player") then
             local nameplate = C_NamePlate.GetNamePlateForUnit(player.targetId)
             if nameplate and player.texture then
                 local targetGUID  = UnitGUID(player.targetId)
                 local targetCount = getTargetCount(targetGUID)
 
-                -- create a frame to hold all the textures on the nameplate
                 local width, height = player.texture:GetSize()
                 if not nameplateFrames[targetGUID] then
                     nameplateFrames[targetGUID] = CreateFrame("Frame", nil, nameplate)
 
-                    -- set the overall size and center it
                     local xOffset = Target_Settings.xValue
                     local yOffset = Target_Settings.yValue
     
@@ -146,7 +132,6 @@ function updateNamePlates(self)
                     nameplateFrames[targetGUID]:Show()
                 end
 
-                -- now add the player texture at correct position within frame
                 if currentCounts[targetGUID] then
                     currentCounts[targetGUID] = currentCounts[targetGUID] + 1
                 else
@@ -164,12 +149,10 @@ function updateNamePlates(self)
 end
 
 function initializeUI()
-    -- Create a frame for options
     addon.optionsFrame = CreateFrame("Frame", nil, InterfaceOptionsFramePanelContainer)
     addon.optionsFrame.name = addonName
     InterfaceOptions_AddCategory(addon.optionsFrame)
 
-    -- Create X slider
     addon.xSlider = CreateFrame("Slider", "TargetXSlider", addon.optionsFrame, "OptionsSliderTemplate")
     addon.xSlider:SetPoint("TOP", addon.optionsFrame, "TOP", 0, -20)
     addon.xSlider:SetMinMaxValues(-200, 200)
@@ -180,12 +163,29 @@ function initializeUI()
     addon.xSlider:SetScript("OnValueChanged", function(self, value)
         _G[self:GetName() .. 'Text']:SetText("X Offset: " .. floor(value))
         Target_Settings.xValue = value
+        if addon.xInput:GetText() ~= tostring(value) then
+            addon.xInput:SetText(tostring(value))
+        end
     end)
     addon.xSlider:Show()
 
-    -- Create Y slider
+    addon.xInput = CreateFrame("EditBox", "TargetXInput", addon.optionsFrame, "InputBoxTemplate")
+    addon.xInput:SetPoint("TOP", addon.xSlider, "BOTTOM", 0, -5)
+    addon.xInput:SetSize(80, 20)
+    addon.xInput:SetText(tostring(Target_Settings.xValue))
+    addon.xInput:SetAutoFocus(false)
+    addon.xInput:SetCursorPosition(0)
+    addon.xInput:SetScript("OnEnterPressed", function(self)
+        local value = tonumber(self:GetText()) or Target_Settings.xValue
+        value = math.min(math.max(value, -200), 200)
+        self:SetText(tostring(value))
+        addon.xSlider:SetValue(value)
+        Target_Settings.xValue = value
+    end)
+    addon.xInput:Show()
+
     addon.ySlider = CreateFrame("Slider", "TargetYSlider", addon.optionsFrame, "OptionsSliderTemplate")
-    addon.ySlider:SetPoint("TOP", addon.xSlider, "BOTTOM", 0, -20)
+    addon.ySlider:SetPoint("TOP", addon.xInput, "BOTTOM", 0, -20)
     addon.ySlider:SetMinMaxValues(-200, 200)
     addon.ySlider:SetValue(Target_Settings.yValue)
     addon.ySlider:SetValueStep(1)
@@ -194,15 +194,31 @@ function initializeUI()
     addon.ySlider:SetScript("OnValueChanged", function(self, value)
         _G[self:GetName() .. 'Text']:SetText("Y Offset: " .. floor(value))
         Target_Settings.yValue = value
+        if addon.yInput:GetText() ~= tostring(value) then
+            addon.yInput:SetText(tostring(value))
+        end
     end)
     addon.ySlider:Show()
 
-    -- Create transparent option
+    addon.yInput = CreateFrame("EditBox", "TargetYInput", addon.optionsFrame, "InputBoxTemplate")
+    addon.yInput:SetPoint("TOP", addon.ySlider, "BOTTOM", 0, -5)
+    addon.yInput:SetSize(80, 20)
+    addon.yInput:SetText(tostring(Target_Settings.yValue))
+    addon.yInput:SetAutoFocus(false)
+    addon.yInput:SetCursorPosition(0)
+    addon.yInput:SetScript("OnEnterPressed", function(self)
+        local value = tonumber(self:GetText()) or Target_Settings.yValue
+        value = math.min(math.max(value, -200), 200)
+        self:SetText(tostring(value))
+        addon.ySlider:SetValue(value)
+        Target_Settings.yValue = value
+    end)
+    addon.yInput:Show()
+
     addon.transparentCheck = CreateFrame("CheckButton", "TargetTransparentCheckButton", addon.optionsFrame, "UICheckButtonTemplate")
-    addon.transparentCheck:SetPoint("TOP", addon.ySlider, "BOTTOM", 0, -20)
+    addon.transparentCheck:SetPoint("TOP", addon.yInput, "BOTTOM", 0, -20)
     addon.transparentCheck.text:SetText("Transparent Background")
     addon.transparentCheck:SetChecked(Target_Settings.transparent)
-    -- addon.transparentCheck.text.tooltip = "Swap between class color backgrounds and transparent backgrounds."
     addon.transparentCheck:SetScript("OnClick", function(frame)
         local checked = frame:GetChecked()
         Target_Settings.transparent = checked
@@ -211,11 +227,43 @@ function initializeUI()
     end)
     addon.transparentCheck:Show()
 
-    -- Create Discord link input field
+    addon.classColorCheck = CreateFrame("CheckButton", "TargetClassColorCheckButton", addon.optionsFrame, "UICheckButtonTemplate")
+    addon.classColorCheck:SetPoint("TOP", addon.transparentCheck, "BOTTOM", 0, -20)
+    addon.classColorCheck.text:SetText("Use Class Colors")
+    addon.classColorCheck:SetChecked(Target_Settings.useClassColor)
+    addon.classColorCheck:SetScript("OnClick", function(frame)
+        local checked = frame:GetChecked()
+        Target_Settings.useClassColor = checked
+        initializePlayers()
+        updateNamePlates()
+    end)
+    addon.classColorCheck:Show()
+
+    addon.sizeDropdown = CreateFrame("Frame", "TargetSizeDropdown", addon.optionsFrame, "UIDropDownMenuTemplate")
+    addon.sizeDropdown:SetPoint("TOP", addon.classColorCheck, "BOTTOM", 0, -20)
+    addon.sizeDropdown.initialize = function(self, level)
+        local info = UIDropDownMenu_CreateInfo()
+        for size, dimensions in pairs(presetSizes) do
+            info.text = size
+            info.func = function()
+                Target_Settings.iconSize = size
+                UIDropDownMenu_SetText(addon.sizeDropdown, size)
+                initializePlayers()
+                updateNamePlates()
+            end
+            UIDropDownMenu_AddButton(info, level)
+        end
+    end
+    UIDropDownMenu_SetWidth(addon.sizeDropdown, 100)
+    UIDropDownMenu_SetButtonWidth(addon.sizeDropdown, 124)
+    UIDropDownMenu_JustifyText(addon.sizeDropdown, "CENTER")
+    UIDropDownMenu_SetText(addon.sizeDropdown, Target_Settings.iconSize)
+    addon.sizeDropdown:Show()
+
     addon.discordLinkInput = CreateFrame("EditBox", "TargetDiscordLinkInput", addon.optionsFrame, "InputBoxTemplate")
-    addon.discordLinkInput:SetPoint("TOP", addon.transparentCheck, "BOTTOM", 0, -20)
+    addon.discordLinkInput:SetPoint("TOP", addon.sizeDropdown, "BOTTOM", 0, -20)
     addon.discordLinkInput:SetSize(200, 20)
-    addon.discordLinkInput:SetText("https://discord.gg/dmwegA6Z") -- Set your Discord link here
+    addon.discordLinkInput:SetText("https://discord.gg/dmwegA6Z")
     addon.discordLinkInput:SetAutoFocus(false)
     addon.discordLinkInput:SetCursorPosition(0)
     addon.discordLinkInput:SetScript("OnEscapePressed", function(self)
@@ -223,7 +271,6 @@ function initializeUI()
     end)
     addon.discordLinkInput:Show()
 
-    -- Function to show/hide the options frame
     function addon.optionsFrame:refresh()
         local isVisible = addon.optionsFrame:IsShown()
         addon.optionsFrame:SetShown(not isVisible)
